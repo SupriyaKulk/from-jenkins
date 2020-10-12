cd15e1f6a02d42bec66d7b05157b140ea919032b
supriyakulk <supriyakul059@gmail.com>
2020-10-12 15:22:42 +0530

    For Jenkins

---
+++ src/main/java/MavenProjectForJenkins/MavenProjectForJenkins/App.java
@@ -0,0 +1,13 @@
+package MavenProjectForJenkins.MavenProjectForJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "For jenkins" );
+    }
+}
